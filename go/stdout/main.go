02d806458d45e6313efeca99719c4eba6bab6b43
nbari <nbari@tequila.io>
2016-07-06 19:03:23 +0200

    modified:   stdout/main.go

--- go/stdout/main.go
+++ go/stdout/main.go
@@ -3,16 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
 	"time"
 )
-
-func waitForExit() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
-	<-c
-	os.Exit(0)
-}
 
 func main() {
 
