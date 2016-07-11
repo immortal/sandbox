c24381b45779e7fdee237ec41ab3d7fc425c0987
nbari <nbari@tequila.io>
2016-07-11 23:09:46 +0200

    new file:   main.go

---
+++ go/flock/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+)
+
+func main() {
+	file, err := os.Create("Lock")
+	if err != nil {
+		fmt.Printf("Create: %s\n", err)
+		return
+	}
+	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX+syscall.LOCK_NB)
+	if err != nil {
+		file.Close()
+		fmt.Printf("Flock: %s\n", err)
+		return
+	}
+	fmt.Printf("pid: %d  sleeping...", os.Getpid())
+	time.Sleep(60 * time.Second)
+}
