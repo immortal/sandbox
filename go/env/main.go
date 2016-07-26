a16817e91933016d6f9ca08b0134cb3eb53c64df
nbari <nbari@tequila.io>
2016-07-26 15:36:17 +0200

    new file:   main.go

---
+++ go/env/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+
+	for i := 1; i < 1000; i++ {
+		for _, v := range os.Environ() {
+			fmt.Fprintf(os.Stderr, "%s\n", v)
+		}
+		time.Sleep(time.Second)
+	}
+
+}
