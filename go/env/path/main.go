4f145bba528ab1357d82290b3e843629b6d1b359
nbari <nbari@tequila.io>
2016-08-05 14:36:07 +0200

    modified:   main.go

--- go/env/path/main.go
+++ go/env/path/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	path := os.Getenv("PATH")
-	fmt.Println(fmt.Sprintf("PATH=%s:%s", "~user/foo/bar", path))
+	env := []string{}
+	for _, v := range os.Environ() {
+		if strings.HasPrefix(v, "PATH") {
+			pair := strings.Split(v, "=")
+			env = append(env, fmt.Sprintf("PATH=%s:%s", "~user/foo/bar", pair[1]))
+		} else {
+			env = append(env, v)
+		}
+	}
+	for _, v := range env {
+		println(v)
+	}
 }
