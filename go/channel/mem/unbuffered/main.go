2d1d267b95e2f505b36e4e77002af83af42c41ea
nbari <nbari@tequila.io>
2016-08-13 17:40:16 +0200

    modified:   unbuffered/main.go

--- go/channel/mem/unbuffered/main.go
+++ go/channel/mem/unbuffered/main.go
@@ -1,15 +1,19 @@
 package main
 
+import "time"
+
 var c = make(chan int)
 var a string
 
 func f() {
 	a = "hello, world"
+	println("waiting for int....")
 	<-c
 }
 
 func main() {
 	go f()
+	time.Sleep(10 * time.Second)
 	c <- 0
 	print(a)
 }
