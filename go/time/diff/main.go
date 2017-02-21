9ee7e078d3028d4d3c64935f124f62690b3499b6
nbari <nbari@tequila.io>
2017-02-21 19:13:37 +0100

    modified:   main.go

--- go/time/diff/main.go
+++ go/time/diff/main.go
@@ -9,13 +9,13 @@ func main() {
 	start := time.Unix(1411691219, 0)
 
 	diff := time.Since(start)
+	days := diff / (24 * time.Hour)
 	fmt.Printf("diff = %+v\n", diff)
-	fmt.Printf("days = %d\n", diff/(24*time.Hour))
+	fmt.Printf("days = %d\n", days)
 	hours := diff % (24 * time.Hour)
 	fmt.Printf("hours = %d\n", hours/time.Hour)
 	minutes := hours % time.Hour
 	fmt.Printf("minutes = %d\n", minutes/time.Minute)
 	seconds := int(minutes.Seconds()) % 60
 	fmt.Printf("seconds = %d\n", seconds)
-
 }
