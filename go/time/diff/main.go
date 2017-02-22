2112acb942eccb05303f378300b40e00253e495d
nbari <nbari@tequila.io>
2017-02-22 13:29:17 +0100

    modified:   main.go

--- go/time/diff/main.go
+++ go/time/diff/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"math"
 	"time"
 )
 
 func main() {
 	start := time.Unix(1411691219, 0)
-
 	diff := time.Since(start)
-	days := diff / (24 * time.Hour)
 	fmt.Printf("diff = %s\n", diff)
-	fmt.Printf("days = %d\n", days)
+	fmt.Printf("diff = %s\n", TimeDiff(start))
+}
+
+func TimeDiff(t time.Time) string {
+	diff := time.Since(t)
+	days := diff / (24 * time.Hour)
 	hours := diff % (24 * time.Hour)
-	fmt.Printf("hours = %d\n", hours/time.Hour)
 	minutes := hours % time.Hour
-	fmt.Printf("minutes = %d\n", minutes/time.Minute)
-	seconds := int(minutes.Seconds()) % 60
-	fmt.Printf("seconds = %d\n", seconds)
+	seconds := math.Mod(minutes.Seconds(), 60)
+	var buffer bytes.Buffer
+	if days > 0 {
+		buffer.WriteString(fmt.Sprintf("%dd", days))
+	}
+	if hours/time.Hour > 0 {
+		buffer.WriteString(fmt.Sprintf("%dh", hours/time.Hour))
+	}
+	if minutes/time.Minute > 0 {
+		buffer.WriteString(fmt.Sprintf("%dm", minutes/time.Minute))
+	}
+	if seconds > 0 {
+		buffer.WriteString(fmt.Sprintf("%.1fs", seconds))
+	}
+	return buffer.String()
 }
