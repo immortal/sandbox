259f2639ff4ae4c6a76e5159a7c09553ef951b3a
nbari <nbari@tequila.io>
2017-04-07 22:15:02 +0200

    new file:   main.go

---
+++ go/kqueue/watchdir/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("usage: %s /path", os.Args[0])
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	kq, err := syscall.Kqueue()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ev1 := syscall.Kevent_t{
+		Ident:  uint64(file.Fd()),
+		Filter: syscall.EVFILT_VNODE,
+		Flags:  syscall.EV_ADD | syscall.EV_ENABLE | syscall.EV_ONESHOT,
+		Fflags: syscall.NOTE_DELETE | syscall.NOTE_WRITE | syscall.NOTE_EXTEND | syscall.NOTE_ATTRIB | syscall.NOTE_LINK | syscall.NOTE_RENAME | syscall.NOTE_REVOKE,
+		Data:   0,
+		Udata:  nil,
+	}
+
+	//Loop:
+	// wait for events
+	for {
+		// create kevent
+		events := make([]syscall.Kevent_t, 1)
+		n, err := syscall.Kevent(kq, []syscall.Kevent_t{ev1}, events, nil)
+		if err != nil {
+			log.Println("Error creating kevent")
+		}
+		// check if there was an event
+		for {
+			fmt.Printf("n = %+v\n", n)
+			if n > 0 {
+				fmt.Printf("n = %+v\n", n)
+				return
+			}
+		}
+	}
+
+	fmt.Println("fin")
+}
