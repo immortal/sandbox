fa1770c05cafe3147b45d87400bde12700d71062
nbari <nbari@tequila.io>
2018-09-16 21:54:05 +0200

    sync Sun Sep 16 21:54:05 CEST 2018

---
+++ go/socket/close3/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/nbari/violetear"
+)
+
+func catchAll(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I'm catching all\n"))
+}
+
+func main() {
+	l, err := net.Listen("unix", "/tmp/immortal.sock")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+
+	router := violetear.New()
+	router.Verbose = false
+	router.HandleFunc("*", catchAll)
+
+	srv := &http.Server{Handler: router}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		log.Println(srv.Serve(l))
+		wg.Done()
+	}()
+
+	closeSocket := make(chan struct{})
+
+	go func() {
+		<-closeSocket
+		if err := srv.Close(); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		log.Println("fin")
+	}()
+
+	wg.Add(1)
+	go func() {
+		<-closeSocket
+		log.Println("just waiting")
+		wg.Done()
+	}()
+
+	<-time.After(2 * time.Second)
+	log.Println("close socket")
+	close(closeSocket)
+	wg.Wait()
+}
