d0b681e073e16e682b86287ec39f4aab5b88b0ae
nbari <nbari@tequila.io>
2018-09-18 21:35:27 +0200

    mutex

--- go/socket/race/main.go
+++ go/socket/race/main.go
@@ -5,35 +5,30 @@ import (
 	"log"
 	"net/http"
 	"sync"
-	"sync/atomic"
-
-	"github.com/nbari/violetear"
+	"time"
 )
 
 type Counter struct {
-	count uint32
+	count uint
 	flag  bool
+	mu    sync.Mutex
 	quit  chan struct{}
+	time  time.Time
 	wg    sync.WaitGroup
 }
 
-type Status struct {
-	Count uint32 `json:"count"`
-	Flag  bool   `json:"flag"`
-}
-
 func (c *Counter) Start() {
-	atomic.AddUint32(&c.count, 1)
+	c.count = 1
+	c.time = time.Now()
+	c.flag = true
 }
 
 func (c *Counter) Listen() {
-	router := violetear.New()
-	router.Verbose = false
-	router.HandleFunc("/", c.HandleStatus)
 	srv := &http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: http.DefaultServeMux,
 	}
+	http.HandleFunc("/", c.HandleStatus)
 	c.wg.Add(1)
 	go func() {
 		defer c.wg.Done()
@@ -42,17 +37,23 @@ func (c *Counter) Listen() {
 	go func(quit chan struct{}) {
 		<-quit
 		if err := srv.Close(); err != nil {
-			log.Printf("HTTP socket close error: %v", err)
+			log.Printf("HTTP error: %v", err)
 		}
 	}(c.quit)
 }
 
 func (c *Counter) HandleStatus(w http.ResponseWriter, r *http.Request) {
-	status := Status{
-		Count: atomic.LoadUint32(&c.count),
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	status := struct {
+		Count uint   `json:"count"`
+		Flag  bool   `json:"flag"`
+		Time  string `json:"time"`
+	}{
+		Count: c.count,
+		Time:  c.time.UTC().Format(time.RFC3339),
 		Flag:  c.flag,
 	}
-	// return status in json
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(status); err != nil {
 		log.Println(err)
@@ -61,11 +62,23 @@ func (c *Counter) HandleStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	counter := &Counter{
+	c := &Counter{
 		quit: make(chan struct{}),
 	}
-	counter.Start()
-	counter.Listen()
+	c.Start()
+	c.Listen()
+	timeout := time.After(time.Minute)
 	for {
+		select {
+		case <-time.After(time.Second):
+			c.mu.Lock()
+			c.count += 1
+			c.flag = !c.flag
+			c.mu.Unlock()
+		case <-timeout:
+			close(c.quit)
+			c.wg.Wait()
+			return
+		}
 	}
 }
