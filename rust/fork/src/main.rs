9e907ed32cecebfbcfb660abfa3a02cce176d85c
nbari <nbari@tequila.io>
2019-10-24 00:52:07 +0200

    sync Thu Oct 24 00:52:07 CEST 2019

--- rust/fork/src/main.rs
+++ rust/fork/src/main.rs
@@ -1,15 +1,13 @@
-use nix::unistd::{fork, setsid, ForkResult};
-use std::process;
+use fork::{daemon, Fork};
+use std::process::{id, Command};
 
+// ps -axo ppid,pid,pgid,sess,tty,tpgid,stat,uid,user,%mem,%cpu,command, | egrep "fork|sleep|PID"
 fn main() {
-    if let Ok(ForkResult::Child) = fork() {
-        let pid = setsid().expect("sesid failed");
-        if let Ok(ForkResult::Child) = fork() {
-            println!("PGID: {}, my pid {}", pid, process::id());
-            process::Command::new("sleep")
-                .arg("300")
-                .output()
-                .expect("failed to execute process");
-        }
+    if let Ok(Fork::Child) = daemon() {
+        println!("my pid {}", id());
+        Command::new("sleep")
+            .arg("300")
+            .output()
+            .expect("failed to execute process");
     }
 }
