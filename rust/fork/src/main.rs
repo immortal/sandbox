253d194cdb44e8e9abb250e67008c0afae44f8e7
nbari <nbari@tequila.io>
2019-10-25 23:08:56 +0200

    sync Fri Oct 25 23:08:56 CEST 2019

--- rust/fork/src/main.rs
+++ rust/fork/src/main.rs
@@ -3,8 +3,9 @@ use std::process::{id, Command};
 
 // ps -axo ppid,pid,pgid,sess,tty,tpgid,stat,uid,user,%mem,%cpu,command, | egrep "fork|sleep|PID"
 fn main() {
-    if let Ok(Fork::Child) = daemon(false) {
-        println!("my pid {}", id());
+    if let Ok(Fork::Child) = daemon(false, false) {
+        // println!("my pid {}", id());
+        // can't print no stdout/stderr unles daemon(false, true)
         Command::new("sleep")
             .arg("300")
             .output()
