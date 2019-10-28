761645c11399b15f624104accdf39688faf96a04
nbari <nbari@tequila.io>
2019-10-28 10:20:40 +0100

    sync Mon Oct 28 10:20:40 CET 2019

--- rust/fork/src/main.rs
+++ rust/fork/src/main.rs
@@ -1,12 +1,20 @@
-use fork::{daemon, Fork};
+use fork::{daemon, fork, Fork};
 use std::io;
 use std::process::Command;
 
 // ps -axo ppid,pid,pgid,sess,tty,tpgid,stat,uid,user,%mem,%cpu,command, | egrep "fork|sleep|PID"
 fn main() {
+    if let Ok(Fork::Child) = fork() {
+        println!("normal fork");
+        Command::new("sleep")
+            .arg("500")
+            .output()
+            .expect("failed to execute process");
+    }
+
     // read from stdin daemon(false, true)
     // if false will not fork
-    if let Ok(Fork::Child) = daemon(false, false) {
+    if let Ok(Fork::Child) = daemon(false, true) {
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
@@ -23,5 +31,5 @@ fn main() {
                 .output()
                 .expect("failed to execute process");
         }
-    }
+    };
 }
